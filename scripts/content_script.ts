dcf6afce07c7f3b5967f52aa4cfdcdb0854198a2
jtaeyeon05 <jtaeyeon05@gmail.com>
2025-06-25 03:07:17 +0900

    Feature: firefox compatibility, content_script for lcms.skku.edu

---
+++ scripts/content_script.ts
@@ -0,0 +1,5 @@
+console.log("WOW ITS ME", window.location.href)
+console.log("WOW ITS ME2", document.documentElement.getHTML())
+console.log("")
+
+// https://lcms.skku.edu/em/641d33964ca50?startat=0.00&endat=4703.35&TargetUrl=https%3A%2F%2Fcanvas.skku.edu%2Flearningx%2Fapi%2Fv1%2Fcourses%2F63267%2Fsections%2F0%2Fcomponents%2F10348%2Fprogress%3Fuser_id%3D2024312074%26content_id%3D641d33964ca50%26content_type%3Dmovie&sl=1&pr=1&mxpr=2.00&lg=ko
--- scripts/popup.ts
+++ scripts/popup.ts
@@ -1,5 +1,33 @@
+console.log("dsaf")
+
+function inspect(tabs: browser.tabs.Tab[]): boolean {
+    let tab = tabs[0]
+    log({ msg: `[inspect] Started: ${tab.url}` })
+
+    if (!tab.url || !tab.id
+        || (!tab.url.startsWith("https://canvas.skku.edu/courses/") && !tab.url.startsWith("http://canvas.skku.edu/courses/"))
+        || (!tab.url.endsWith("/297") && !tab.url.endsWith("/297/"))
+        || !tab.url.includes("/items/")) {
+        log({msg: "[inspect] Not Expected URL"})
+        return false
+    }
+
+    let iframe = document.querySelector("#tool_content")
+    if (!iframe) {
+        log({msg: "[inspect] iframe Noe Exist"})
+        //return false
+    }
+
+    let a = document.getElementById("downloadButton") as HTMLButtonElement
+    console.log("dafs")
+    return true
+}
+
+
 function log({ msg, sub = false }: { msg: string, sub?: boolean }): void {
-    const logArea = !sub ? document.getElementById("log") as HTMLDivElement : document.getElementById("subLog") as HTMLDivElement
+    const logArea = !sub ?
+        document.getElementById("log") as HTMLDivElement :
+        document.getElementById("subLog") as HTMLDivElement
     const time = new Date().toLocaleTimeString()
     const logLine = `[${time}] ${msg}`
 
@@ -13,9 +41,37 @@ function log({ msg, sub = false }: { msg: string, sub?: boolean }): void {
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    const btn = document.getElementById("downloadBtn") as HTMLButtonElement
-    btn.addEventListener("click", () => {
-        log({ msg: "Button Clicked", sub: true })
+    const inspectButton = document.getElementById("inspectButton") as HTMLButtonElement
+    inspectButton.addEventListener("click", () => {
+        log({ msg: "Inspect Button Clicked", sub: true })
+        browser.tabs.query({ active: true, currentWindow: true }).then(
+            inspect, (tabs: browser.tabs.Tab[]) => { log({ msg: "Tab Query Error" }) }
+        )
+    })
+
+    const downloadButton = document.getElementById("downloadButton") as HTMLButtonElement
+    downloadButton.addEventListener("click", () => {
+        log({ msg: "Download Button Clicked", sub: true })
+        //
+    })
+})
+
+log({ msg: "TS Loaded", sub: true })
+
+/*
+document.addEventListener("DOMContentLoaded", () => {
+    const inspectButton = document.getElementById("inspectButton") as HTMLButtonElement
+    inspectButton.addEventListener("click", () => {
+        log({ msg: "Inspect Button Clicked", sub: true })
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            const tab = tabs[0]
+            if (tab) inspect(tab)
+        })
+    })
+
+    const downloadButton = document.getElementById("downloadButton") as HTMLButtonElement
+    downloadButton.addEventListener("click", () => {
+        log({ msg: "Download Button Clicked", sub: true })
         chrome.runtime.sendMessage({
             command: "downloadICampus",
             url: "https://vod.skku.edu/contents4/skku100001/646d582510df8/contents/media_files/mobile/ssmovie.mp4",
@@ -23,5 +79,4 @@ document.addEventListener("DOMContentLoaded", () => {
         })
     })
 })
-
-log({ msg: "TS Loaded", sub: true })
+ */
